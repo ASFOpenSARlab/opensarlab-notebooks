5f91d5ff1caeb53f0da2b88a352a97e687a1c021
Alex-Lewandowski <37909088+Alex-Lewandowski@users.noreply.github.com>
2019-07-30 10:05:24 -0800

    First major release of updated labs and non-lab recipes (#22)
    * Add refactored lab 3 and lab 4
    Not yet completed
    
    * update Lab3, 4. Add Lab 4B, 5
    
    * backup lab 3, 4, 4b, 5. add lab 6
    
    * update lab 3 and 5
    
    * update labs 3, 4, 4B
    started working on 4B, using my newly created Hyp subscription. Refactored
    strings using Python3's new f-string format.
    
    * update Lab 4B
    refactoring ~75% complete
    
    * update Labs 4, 4B, 5, 6
    
    * add Learn_Vertex_API.ipynb
    
    * add asf_notebook.py
    
    * change data_level to processing_level
    add processingLevel param to post request instead of using if stmnts
    
    * update Lab4B, 5(about done), and asf_notebook.py
    
    * update Labs 4, 5. asf_notebook.py
    
    * updates Lab_4B_dev and asf_notebook.py
    
    * update lab4B.ipynb
    
    * update Labs 4,4B,5,6,8 and asf_notebook.py
    
    * update asf_notebook.py add Lab8_dev.ipynb
    
    * update Lab8_dev
    
    * bring lab8 to stage 1
    
    * Update Labs 3,4,4B,5,6,7_cosine,7_Taizhou, 8, 8_own, and asf-notebook.py
    
    * add filter by path and flight direction
    
    * add Lab9_Hyp3_dev
    
    * update lab4B, lab8_own, and asf_notebook.py
    
    * update all labs except 3
    
    * update Lab4 add recipe_notebooks
    
    * remove ASF/GEOS_657_Labs/Lab4-copy2.ipynb
    
    * remove ASF/GEOS_657_Labs/Lab4B-copy2.ipynb
    
    * remove ASF/GEOS_657_Labs/Lab4B-dev.ipynb
    
    * remove ASF/GEOS_657_Labs/Lab5-Copy2.ipynb
    
    * remove ASF/GEOS_657_Labs/Lab6_copy2.ipynb
    
    * remove GEOS 657-Lab3-SARProcessingAndGeocoding-Copy1.ipynb
    
    * remove GEOS 657-Lab4-SARTimeSeriesAnalysis-Copy1.ipynb
    
    * remove GEOS 657-Lab4B-SARTimeSeriesAnalysis-OwnData-Copy1.ipynb
    
    * remove GEOS 657-Lab5-InSARwithSNAP-Copy1.ipynb
    
    * remove GEOS 657-Lab6-VolcanoSourceModelingfromInSAR-Copy1.ipynb
    
    * rename recipe_notebooks to Recipe_Notebooks
    
    * delete Recipe_Notebooks/Time_Series_Example.ipynb
    
    * add Recipe_Notebooks
    
    * add remaining recipe notebooks
    
    * update lab4*, 8_own and non-lab versions of same. asf_notebook.py
    
    * update frame merge in lab4B, 8, and non-lab versions
    
    * update all notebooks with final changes for pull request
    
    * removed data_change_detection_amplitude_time_series_hyp3.ipynb
    
    * removed dev copies of labs
    
    * remove lab9_hyp3.dev
    
    * add SAR_Training and recipes to NISAR
    
    * Update Readme to Spanish
    
    * update all copies asf_notebook.py
    
    * update all copies asf_notebook.py
    -date_range_valid(): return False for None dates
    -flight_direction_valid(): return False for None direction
    -refactor download_hyp3_products() to account for refactored date_range_valid
    and flight_direction_valid()
    -add type hints to get_vertex_granule_info()
    
    * update all notebooks
    -organize import statements
    -set GDAL raster variables to None when done with them
    -Test all notebooks to confirm that they run successfully
    
    * update storage paths in NISAR, SAR_Training/English notebooks
    
    * remove NISAR/Recipes
    
    * remove ASF/Recipe_Notebooks
    
    * add supporting docs to SAR_Training/English

---
+++ SAR_Training/English/makeSNAPIntf.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+
+import re,sys,os
+import numpy as np
+import saa_func_lib as saa
+
+
+i = sys.argv[1]
+q = sys.argv[2]
+
+(x,y,trans,proj,idata) = saa.read_gdal_file(saa.open_gdal_file(i))
+(x,y,trans,proj,qdata) = saa.read_gdal_file(saa.open_gdal_file(q))
+
+#amp = np.sqrt(np.sqrt(np.power(idata,2) + np.power(qdata,2)))
+amp = np.log10((np.sqrt(np.sqrt(np.power(idata,2) + np.power(qdata,2)))+1)/100000000)
+amp = (amp - np.min(amp))
+ma = amp[amp != 0]
+amp = amp - np.min(ma)
+#amp = np.where(amp < 0, amp, 0)
+amp = amp/np.max(amp)*255
+phase = (np.arctan2(qdata,idata)+np.pi)/(2*np.pi)*255
+
+
+saa.write_gdal_file_byte('amplitude.tif',trans,proj,amp)
+saa.write_gdal_file_byte('phase.tif',trans,proj,phase)
+
+
+
+
+
+
+
+
+
