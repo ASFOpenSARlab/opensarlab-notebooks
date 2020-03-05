310fb691dd34abf0a92189c0e5a47add0c165219
Alex-Lewandowski <aflewandowski@alaska.edu>
2020-03-05 14:19:34 -0900

    add vrt_to_gtiff function

--- ASF/GEOS_657_Labs/asf_notebook.py
+++ ASF/GEOS_657_Labs/asf_notebook.py
@@ -163,7 +163,27 @@ def handle_old_data(data_dir, contents):
              continue
         return selection
 
+              
+###################
+#  GDAL Functions #
+###################
+              
+def vrt_to_gtiff(vrt: str, output: str):
+    if '.vrt' not in vrt:
+        print('Error: The path to your vrt does not contain a ".vrt" extension.')
+        return
+    if '.' not in output:
+        output = f"{output}.tif"
+    elif len(output) > 4 and (output[:-3] == 'tif' or output[:-4] == 'tiff'):
+        print('Error: the output argument must either not contain a ' /
+              'file extension, or have a "tif" or "tiff" file extension.')
+        return
         
+    cmd = f"gdal_translate -co \"COMPRESS=DEFLATE\" -a_nodata 0 {vrt} {output}"
+    sub = subprocess.run(cmd, stderr=subprocess.PIPE, shell=True)
+    print(str(sub.stderr)[2: -3])
+         
+     
 ########################
 #  Earth Data Function #
 ########################
