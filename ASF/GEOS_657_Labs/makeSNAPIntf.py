ab5d4ec88eaec695f52b98810a099280ef841576
Alex-Lewandowski <aflewandowski@alaska.edu>
2020-04-18 09:10:20 -0800

    replace makeSNAPIntf.py with version producing phase.tif

--- ASF/GEOS_657_Labs/makeSNAPIntf.py
+++ ASF/GEOS_657_Labs/makeSNAPIntf.py
@@ -1,31 +1,24 @@
 #!/usr/bin/env python
 
 
-import re,sys,os,glob
+import re,sys,os
 import numpy as np
 import saa_func_lib as saa
 
-def gdal_interferogram(folder_in=None, name=''):
-    if folder_in is None:
-        folder_in = os.getcwd()
 
-    qfile = glob.glob(os.path.join(folder_in, 'q_*.img'))[0]
-    ifile = qfile.replace('q_', 'i_')
-    (x,y,trans,proj,idata) = saa.read_gdal_file(saa.open_gdal_file(ifile))
-    (x,y,trans,proj,qdata) = saa.read_gdal_file(saa.open_gdal_file(qfile))
+i = sys.argv[1]
+q = sys.argv[2]
 
-    #amp = np.sqrt(np.sqrt(np.power(idata,2) + np.power(qdata,2)))
-    amp = np.log10((np.sqrt(np.sqrt(np.power(idata,2) + np.power(qdata,2)))+1)/100000000)
-    amp = (amp - np.min(amp))
-    ma = amp[amp != 0]
-    amp = amp - np.min(ma)
-    #amp = np.where(amp < 0, amp, 0)
-    amp = amp/np.max(amp)*255
-    phase = (np.arctan2(qdata,idata)+np.pi)/(2*np.pi)*255
+(x,y,trans,proj,idata) = saa.read_gdal_file(saa.open_gdal_file(i))
+(x,y,trans,proj,qdata) = saa.read_gdal_file(saa.open_gdal_file(q))
 
 
-    saa.write_gdal_file_byte(os.path.join(folder_in, f'amplitude{name}.tif'),trans,proj,amp)
-    saa.write_gdal_file_byte(os.path.join(folder_in, f'phase{name}.tif'),trans,proj,phase)
+amp = np.sqrt(np.sqrt(np.power(idata,2) + np.power(qdata,2)))
+phase = np.arctan2(qdata,idata)
+
+
+saa.write_gdal_file_float('amplitude.tif',trans,proj,amp)
+saa.write_gdal_file_float('phase.tif',trans,proj,phase)
 
 
 
